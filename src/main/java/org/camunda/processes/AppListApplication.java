1649f1b6278032edfb94234758748450bf991187
Patricio Soto <psoto@napsis.cl>
2017-07-22 08:04:43 -0400

    limpieza

--- src/main/java/org/camunda/processes/AppListApplication.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.processes;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("AppList")
-public class AppListApplication extends ServletProcessApplication {
-  // empty implementation
-}
